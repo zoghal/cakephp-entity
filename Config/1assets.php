850a4d67b2c16b40a4bb7f0074de8162c42675aa
zoghal <saleh.souzanchi@gmail.com>
2014-04-06 18:15:46 +0430

    ready to use rita

---
+++ Config/1assets.php
@@ -0,0 +1,43 @@
+<?php
+return[
+	'admin' =>  [
+		'script' => [	
+			'lt' => [
+
+			],
+			'lb' => [
+
+			],
+			'ct' => [],
+			'cb' => [
+				
+			]
+		],
+		'style' => [	
+			'lt' => [
+				'UserManager.icon/user-icon',
+				'UserManager.icon2/flaticon',
+				'UserManager.user-manager'
+			],
+			'lb' => [],
+			'ct' => [],
+			'cb' => []
+		]
+	],
+	'front' => [
+		'script' => [	
+			'lt' => [],
+			'lb' => [],
+			'ct' => [],
+			'cb' => []
+		],
+		'style' => [	
+			'lt' => [],
+			'lb' => [],
+			'ct' => [],
+			'cb' => []
+		]
+	
+	]
+
+];
